19e6c058338db2c5ed28112ad9fbfbe1abfdc7f2
m4iple <75981179+m4iple@users.noreply.github.com>
2025-07-16 19:47:33 +0200

    feat: updted gui, start implemention of VAD

--- s2s.py
+++ s2s.py
@@ -1,15 +1,22 @@
 import sounddevice as sd
+from silero_vad import load_silero_vad, read_audio, get_speech_timestamps
 
 class S2S:
     def __init__(self):
         self.stream = None
-        self.input = sd.default.device[0]
-        self.output = sd.default.device[1]
-        self.samplerate = sd.default.samplerate
+        self.input = sd.default.device[0] #71
+        self.output = sd.default.device[1] #51
+        self.samplerate = sd.default.samplerate if sd.default.samplerate is not None else 44100
         self.blocksize = sd.default.blocksize
         self.dtype = sd.default.dtype
-        self.latency = [0.05, 0.05]
+        self.latency = ['low', 'low']
         self.channels = sd.default.channels
+        if self.channels is None or self.channels == [None, None]:
+            self.channels = [1, 1]
+        else:
+            self.channels = [c if c is not None else 1 for c in self.channels]
+
+        self.avd_model = load_silero_vad()
 
     def get_audio_devices(self):
         return sd.query_devices()
@@ -45,36 +52,52 @@ class S2S:
         self.samplerate = samplerate
 
     def set_blocksize(self, blocksize):
-        self.blocksize = [blocksize, blocksize]
+        self.blocksize = blocksize
 
     def set_dtype(self, dtype):
         self.dtype = [dtype, dtype]
 
     def set_latency(self, latency):
+        if isinstance(latency, int):
+            latency_map = {0: 'low', 1: 'high'}
+            latency = latency_map.get(latency, 'low')
         self.latency = [latency, latency]
-        print(self.latency)
 
     def set_channels(self, channels):
         self.channels = [channels, channels]
 
     def callback(self, indata, outdata, frames, time, status):
+        self.print_timing()
+        # create an audio queu and then trimm it with silero_vad
         outdata[:] = indata 
 
     def start_stream(self):
         if self.stream is None:
-            self.stream = sd.Stream(
-                device=(self.input, self.output),
-                samplerate=self.samplerate,
-                blocksize=self.blocksize,
-                dtype=self.dtype,
-                latency=self.latency,
-                channels=self.channels,
-                callback=self.callback
-            )
-            self.stream.start()
+            wasapi_exclusive = sd.WasapiSettings(exclusive=True)
+            try:
+                self.stream = sd.Stream(
+                    device=(self.input, self.output),
+                    samplerate=self.samplerate,
+                    blocksize=self.blocksize,
+                    dtype=self.dtype,
+                    latency=self.latency,
+                    channels=self.channels,
+                    callback=self.callback,
+                )
+                self.stream.start()
+            except Exception as e:
+                print(f'Stream exception {e}')
 
     def end_stream(self):
         if self.stream is not None:
             self.stream.stop()
             self.stream.close()
             self.stream = None
+
+    def print_timing(self):
+        input_lat = self.stream.latency[0]
+        output_lat = self.stream.latency[1]
+
+        print(f"Reported Input Latency: {input_lat * 1000:.2f} ms")
+        print(f"Reported Output Latency: {output_lat * 1000:.2f} ms")
+        print(f"Total Reported Latency: {(input_lat + output_lat) * 1000:.2f} ms")
--- ui.py
+++ ui.py
@@ -24,14 +24,40 @@ class StreamWindow(QMainWindow):
         self.output_combo.setCurrentIndex(default_output)
         self.output_combo.currentIndexChanged.connect(self.change_output)
 
+        self.latency_combo = QComboBox()
+        self.latency_combo.addItems(['Low', 'High'])
+        self.latency_combo.setCurrentIndex(0)
+        self.latency_combo.currentIndexChanged.connect(self.change_latency)
 
-        self.latency_spin = QDoubleSpinBox()
-        self.latency_spin.setDecimals(2)
-        self.latency_spin.setSingleStep(0.01)
-        self.latency_spin.setMinimum(0.01)
-        self.latency_spin.setMaximum(1.0)
-        self.latency_spin.setValue(float(self.s2s.get_current_latency()))
-        self.latency_spin.valueChanged.connect(self.change_latency)
+        # Samplerate
+        self.samplerate_spin = QDoubleSpinBox()
+        self.samplerate_spin.setRange(8000, 192000)
+        self.samplerate_spin.setValue(self.s2s.get_current_samplerate())
+        self.samplerate_spin.setSingleStep(1000)
+        self.samplerate_spin.valueChanged.connect(self.change_samplerate)
+
+        # Blocksize
+        self.blocksize_spin = QDoubleSpinBox()
+        self.blocksize_spin.setRange(16, 8192)
+        self.blocksize_spin.setValue(self.s2s.get_current_blocksize())
+        self.blocksize_spin.setSingleStep(16)
+        self.blocksize_spin.valueChanged.connect(self.change_blocksize)
+
+        # Dtype
+        self.dtype_combo = QComboBox()
+        dtypes = ['float32', 'int16', 'int32', 'uint8']
+        self.dtype_combo.addItems(dtypes)
+        current_dtype = str(self.s2s.get_current_dtype())
+        if current_dtype in dtypes:
+            self.dtype_combo.setCurrentText(current_dtype)
+        self.dtype_combo.currentTextChanged.connect(self.change_dtype)
+
+        # Channels
+        self.channels_spin = QDoubleSpinBox()
+        self.channels_spin.setRange(1, 16)
+        self.channels_spin.setValue(self.s2s.get_current_channels())
+        self.channels_spin.setSingleStep(1)
+        self.channels_spin.valueChanged.connect(self.change_channels)
 
         self.toggle_btn = QPushButton('Start Stream')
         self.toggle_btn.setCheckable(True)
@@ -47,8 +73,19 @@ class StreamWindow(QMainWindow):
 
         row2 = QHBoxLayout()
         row2.addWidget(QLabel('Latency:'))
-        row2.addWidget(self.latency_spin)
+        row2.addWidget(self.latency_combo)
+        row2.addWidget(QLabel('Samplerate:'))
+        row2.addWidget(self.samplerate_spin)
+        row2.addWidget(QLabel('Blocksize:'))
+        row2.addWidget(self.blocksize_spin)
         layout.addLayout(row2)
+
+        row4 = QHBoxLayout()
+        row4.addWidget(QLabel('Dtype:'))
+        row4.addWidget(self.dtype_combo)
+        row4.addWidget(QLabel('Channels:'))
+        row4.addWidget(self.channels_spin)
+        layout.addLayout(row4)
 
         layout.addWidget(self.toggle_btn)
 
@@ -58,25 +95,50 @@ class StreamWindow(QMainWindow):
 
     def change_input(self):
         index = self.input_combo.currentIndex()
-        self.s2s.set_new_input(index)
+        self.s2s.set_input(index)
 
     def change_output(self):
         index = self.output_combo.currentIndex()
-        self.s2s.set_new_output(index)
+        self.s2s.set_output(index)
 
     def change_latency(self):
-        latency = self.latency_spin.value()
-        self.s2s.set_latency(latency)
-        
+        index = self.latency_combo.currentIndex()
+        self.s2s.set_latency(index)
+
+    def change_samplerate(self):
+        value = int(self.samplerate_spin.value())
+        self.s2s.set_samplerate(value)
+
+    def change_blocksize(self):
+        value = int(self.blocksize_spin.value())
+        self.s2s.set_blocksize(value)
+
+    def change_dtype(self):
+        dtype = self.dtype_combo.currentText()
+        self.s2s.set_dtype(dtype)
+
+    def change_channels(self):
+        value = int(self.channels_spin.value())
+        self.s2s.set_channels(value)
 
     def toggle_stream(self):
         if self.toggle_btn.isChecked():
             self.toggle_btn.setText('Stop Stream')
             self.input_combo.setDisabled(True)
             self.output_combo.setDisabled(True)
+            self.latency_combo.setDisabled(True)
+            self.samplerate_spin.setDisabled(True)
+            self.blocksize_spin.setDisabled(True)
+            self.dtype_combo.setDisabled(True)
+            self.channels_spin.setDisabled(True)
             self.s2s.start_stream()
         else:
             self.toggle_btn.setText('Start Stream')
             self.input_combo.setDisabled(False)
             self.output_combo.setDisabled(False)
+            self.latency_combo.setDisabled(False)
+            self.samplerate_spin.setDisabled(False)
+            self.blocksize_spin.setDisabled(False)
+            self.dtype_combo.setDisabled(False)
+            self.channels_spin.setDisabled(False)
             self.s2s.end_stream()
