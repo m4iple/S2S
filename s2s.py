ba2a89724cc9ad11df5bab66dcda134e399a8858
m4iple <75981179+m4iple@users.noreply.github.com>
2025-07-21 18:38:32 +0200

    feat: added vad proccesing of the audio stream

--- s2s.py
+++ s2s.py
@@ -1,13 +1,17 @@
 import sounddevice as sd
-from silero_vad import load_silero_vad, read_audio, get_speech_timestamps
+import time
+import torch
+import torchaudio
+from silero_vad import load_silero_vad
 
 class S2S:
     def __init__(self):
+        self.debug_printend = False
         self.stream = None
         self.input = sd.default.device[0] #71
         self.output = sd.default.device[1] #51
         self.samplerate = sd.default.samplerate if sd.default.samplerate is not None else 44100
-        self.blocksize = sd.default.blocksize
+        self.blocksize = 1024
         self.dtype = sd.default.dtype
         self.latency = ['low', 'low']
         self.channels = sd.default.channels
@@ -16,7 +20,14 @@ class S2S:
         else:
             self.channels = [c if c is not None else 1 for c in self.channels]
 
-        self.avd_model = load_silero_vad()
+        self.vad_model = load_silero_vad()
+        self.vad_samplerate = 16000
+        self.vad_speech_prob = 0.5
+        self.vad_buffer_lenght = 512
+        self.vad_audio_buffer = torch.tensor([])
+
+        self.speech_audio_buffer = torch.tensor([])
+
 
     def get_audio_devices(self):
         return sd.query_devices()
@@ -67,13 +78,13 @@ class S2S:
         self.channels = [channels, channels]
 
     def callback(self, indata, outdata, frames, time, status):
-        self.print_timing()
-        # create an audio queu and then trimm it with silero_vad
-        outdata[:] = indata 
+        self.print_stream_device_timing()
+        processed = self.process_audio(indata)
+        outdata[:] = processed 
 
     def start_stream(self):
         if self.stream is None:
-            wasapi_exclusive = sd.WasapiSettings(exclusive=True)
+            #wasapi_exclusive = sd.WasapiSettings(exclusive=True) # not avaiable for my device (for now)
             try:
                 self.stream = sd.Stream(
                     device=(self.input, self.output),
@@ -93,11 +104,47 @@ class S2S:
             self.stream.stop()
             self.stream.close()
             self.stream = None
+    
+    def process_audio(self, indata):
+        debug_speech_detect_start = time.time()
+        # idea do later to capure dual chanel audio of original speech to later autotune tts to it?
+        audio_tensor = torch.from_numpy(indata[:, 0]).to(torch.float32)
+        resampled_audio = self.resample_audio(audio_tensor, self.samplerate, self.vad_samplerate)
+        self.vad_audio_buffer = torch.cat([self.vad_audio_buffer, resampled_audio])
 
-    def print_timing(self):
-        input_lat = self.stream.latency[0]
-        output_lat = self.stream.latency[1]
+        while self.vad_audio_buffer.shape[0] > self.vad_buffer_lenght:
+            chunk = self.vad_audio_buffer[:self.vad_buffer_lenght]
+            self.vad_audio_buffer = self.vad_audio_buffer[self.vad_buffer_lenght:]
 
-        print(f"Reported Input Latency: {input_lat * 1000:.2f} ms")
-        print(f"Reported Output Latency: {output_lat * 1000:.2f} ms")
-        print(f"Total Reported Latency: {(input_lat + output_lat) * 1000:.2f} ms")
+            speech_prob = self.vad_model(chunk, self.vad_samplerate).item()
+            if speech_prob > self.vad_speech_prob:
+                self.speech_audio_buffer = torch.cat([self.speech_audio_buffer, chunk])
+                # speech dtect takes about 1.30 - 2.30 ms
+                debug_speech_detect_end = time.time()
+                self.debug_time_print("Speech detect:", debug_speech_detect_start, debug_speech_detect_end)
+                print(f"Speech detected! (Confidence: {speech_prob:.2f})")
+
+        return indata
+
+    def resample_audio(self, audio_tensor, original_rate, target_rate):
+        if original_rate == target_rate:
+            return audio_tensor
+
+        resampler = torchaudio.transforms.Resample(orig_freq=original_rate, new_freq=target_rate)
+        return resampler(audio_tensor)
+
+    def print_stream_device_timing(self):
+        if self.debug_printend == False:
+            self.debug_printend = True
+            input_lat = self.stream.latency[0]
+            output_lat = self.stream.latency[1]
+        
+            print("---")
+            print(f"Reported Input Latency: {input_lat * 1000:.2f} ms")
+            print(f"Reported Output Latency: {output_lat * 1000:.2f} ms")
+            print(f"Total Reported Latency: {(input_lat + output_lat) * 1000:.2f} ms")
+            print("---")
+    
+    def debug_time_print(self, text, debug_time_start, debug_time_end):
+        elapsed_ms = (debug_time_end - debug_time_start) * 1000
+        print(f"{text} {elapsed_ms:.2f} ms")
